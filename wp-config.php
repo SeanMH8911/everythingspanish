d44394f41765a2bff071b698679d47ad1449f296
Sean Howe <55624526+SeanMH8911@users.noreply.github.com>
2022-12-17 10:55:26 +0000

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@ define('WP_CACHE', true); // WP-Optimize Cache
  */
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'everything_spanish' );
+define( 'DB_NAME', 'sql8585177' );
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'sql8585177' );
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'VB94fDYrjb' );
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'sql8.freesqldatabase.com' );
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
 /** The database collate type. Don't change this if in doubt. */
